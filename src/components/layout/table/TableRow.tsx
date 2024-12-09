69eabbd0500ae1be4f3d8675b3ece259dfaa2808
toho36 <toho0036@gmail.com>
2024-12-09 12:58:08 +0100

    clean up

--- src/components/layout/table/TableRow.tsx
+++ src/components/layout/table/TableRow.tsx
@@ -17,13 +17,7 @@ const TableRow: React.FC<TableRowProps> = ({ article, requestDelete }) => (
       </Typography>
     </td>
     <td className="p-4 max-w-[420px]">
-      <Typography
-        variant="small"
-        className="font-normal 
-      
-              line-clamp-1
-      text-gray-600"
-      >
+      <Typography variant="small" className="font-normal line-clamp-1  text-gray-600">
         {article.perex}
       </Typography>
     </td>
